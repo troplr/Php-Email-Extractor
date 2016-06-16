667c8b912ea393ee784c08da53c0c2150c8efcdd
troplr <sandeep.tete2011@gmail.com>
2016-06-16 14:00:28 +0530

    Update extract.php

--- extract.php
+++ extract.php
@@ -198,16 +198,6 @@ $newemail = implode("<br/>",$new);
 <!-- <TEXTAREA id="copyTarget"><?php echo $new;?></TEXTAREA><button id="copyButton">Copy</button> -->
 <!-- <input type="text" id="copyTarget" value="<?php echo (implode ( "<br/>",$emails));?>"> <button id="copyButton">Copy</button>
  -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-79381438-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 </body>
 </html>
